020df7888031a2947c6b0a675f738829386d3583
Oleksander Voron <irreligious86@gmail.com>
2025-02-27 01:15:41 +0200

    Refactored and optimized the code by removing unused sections, redundant logic, and unnecessary fragments for better maintainability.

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,3 @@
-fetch('/static/data/fpv_channels.json')
-    .then(response => response.json())
-    .then(fpvData => {
-        let bandInfo = fpvData["5.8GHz"]["Band A (Boscam)"];
-        console.log("Band Info:", bandInfo);
-        console.log("Channels:", bandInfo.channels);
-    });
-
-function updateChannels() {
-    const bandSelect = document.getElementById("bandSelect");
-    const channelSelect = document.getElementById("channelSelect");
-    channelSelect.innerHTML = "";
-    if (bandSelect.value === "") {
-        channelSelect.disabled = true;
-        channelSelect.innerHTML = "<option value=''>Выберите бенд</option>";
-    } else {
-        channelSelect.disabled = false;
-        bandChannels[bandSelect.value].forEach(channel => {
-            const option = document.createElement("option");
-            option.value = channel;
-            option.textContent = `Channel ${channel}`;
-            channelSelect.appendChild(option);
-        });
-    }
-}
-
-
-
-// 
-
 // Универсальная функция для привязки обработчика к кнопке
 function attachButtonHandler(button, handlerFunction) {
     button.addEventListener("click", handlerFunction);
