6a7984fe3f3e326943c1d4e25923dcd64bc59d40
Oleksander Voron <irreligious86@gmail.com>
2025-03-01 01:05:14 +0200

    Init alternative version v.2.0 of current project

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# # Кешируем JSON при старте сервера
+# with open("static/data/channels.json", "r", encoding="utf-8") as file:
+#     CHANNELS_DATA = json.loads(file)
+
+# @app.route('/get_channels')
+# def get_channels():
+#     """Отдаёт JSON-данные"""
+#     return jsonify(CHANNELS_DATA)
 
 if __name__ == '__main__':
     app.run(debug=True)
